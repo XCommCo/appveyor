5ec3e7edd4598056df2af643cc32f3c5d1a33ae0
Scott Charlton <charlton@usgs.gov>
2018-10-20 01:00:10 -0600

    try double quotes

--- FaSTMECH/Tutorial 1/Exercise 1/fastmech_tut1_ex1.py
+++ FaSTMECH/Tutorial 1/Exercise 1/fastmech_tut1_ex1.py
@@ -113,7 +113,7 @@ pyautogui.screenshot("preProcessingMaximize-{}x{}.png".format(screenWidth, scree
 
 # Import r5finpt2m114_shifted.tpo
 pyautogui.typewrite(['alt', 'i', 'e', 'enter'], interval=.2)
-pyautogui.typewrite(os.getcwd()+'\\r5finpt2m114_shifted.tpo', interval=.2)
+pyautogui.typewrite('"'+os.getcwd()+'\\r5finpt2m114_shifted.tpo'+'"', interval=.2)
 pyautogui.typewrite(['enter'], interval=.2)
 time.sleep(2.5)
 # leave default setting of 1
